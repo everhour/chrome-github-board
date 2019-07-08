fee64736d311ffb99cfc75576cf1cf516908cdf8
Yury Tolochko <yury.tolochko@weavora.com>
2019-07-08 16:18:14 +0300

    compact mode switcher; note card - show menu;

--- src/github-kanban.js
+++ src/github-kanban.js
@@ -1,7 +1,7 @@
-document.body.classList.add('evh-compact');
+document.body.classList.add('ghcp-on');
 
 observeChanges('.issue-card, .js-issue-number, .js-project-card-issue-link', () => {
-    document.querySelectorAll('.issue-card:not(.evh-contains-number)').forEach(card => {
+    document.querySelectorAll('.issue-card:not(.ghcp-contains-number)').forEach(card => {
         const number = card.querySelector('.js-issue-number');
 
         if (!number) {
@@ -11,15 +11,37 @@ observeChanges('.issue-card, .js-issue-number, .js-project-card-issue-link', () 
         const flex = card.children[1];
 
         const numberSpan = document.createElement('SPAN');
-        numberSpan.classList.add('evh-number');
+        numberSpan.classList.add('ghcp-number');
         numberSpan.innerText = number.innerText;
 
         flex.insertBefore(numberSpan, flex.firstChild);
 
-        card.classList.add('evh-contains-number');
+        card.classList.add('ghcp-contains-number');
 
         if (card.querySelector('.issue-card-label[data-card-filter="label:SUPPORT"]')) {
-            card.classList.add('evh-important-issue');
+            card.classList.add('ghcp-important-issue');
         }
     });
+});
+
+observeChanges('.project-header-controls:not(.ghcb-contains-switcher)', () => {
+    const controls = document.querySelector('.project-header-controls');
+
+    const switcher = document.createElement('DIV');
+    switcher.classList.add('ghcb-switcher');
+
+    switcher.innerHTML = `
+        <select id="ghcb-switcher">
+            <option value="on" selected>Compact: ON</option>
+            <option value="off">Compact: OFF</option>
+        </select>
+    `;
+
+    switcher.onchange = () => {
+        const enabled = document.querySelector('#ghcb-switcher').selectedIndex === 0;
+        document.body.classList.toggle('ghcp-on', enabled);
+        document.body.classList.toggle('ghcp-off', !enabled);
+    };
+
+    controls.insertBefore(switcher, controls.firstChild);
 });
