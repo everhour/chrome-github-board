e47060dec61e1f8767a6a02ec889eda8721c2e21
Yury Tolochko <yury.tolochko@weavora.com>
2019-07-16 15:46:13 +0300

    proper zip

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('release', function() {
-    return gulp.src(['./lib/**', './src/**', './logo.png', './manifest.json'])
+    return gulp.src(['./lib/**', './src/**', './logo.png', './manifest.json'], {base: '.'})
         .pipe(zip('build.zip'))
         .pipe(gulp.dest('./dist'));
 });
